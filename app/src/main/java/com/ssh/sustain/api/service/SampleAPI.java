dd2e9ca578739061728e7fb6ef1ef65c54bf0708
SmolKim <gitmiu@gmail.com>
2021-07-22 09:45:51 +0900

    MVC
    MVP Pattern 사용은 철회하고 단순 MVC Pattern을 사용할 예정이다.

---
+++ app/src/main/java/com/ssh/sustain/api/service/SampleAPI.java
@@ -0,0 +1,11 @@
+package com.ssh.sustain.api.service;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
+public interface SampleAPI {
+
+    @POST(value = "/sample")
+    Call<String> sample();
+
+}
